13d78a7e8868362e2791d54a71e7f0ac2de30590
gagneray <50414560+gagneray@users.noreply.github.com>
2021-05-12 21:05:02 +0200

    init springboot project

---
+++ src/main/java/org/gagneray/api/banditproblemapi/BanditproblemApiApplication.java
@@ -0,0 +1,13 @@
+package org.gagneray.api.banditproblemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanditproblemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanditproblemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gagneray/api/banditproblemapi/BanditproblemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gagneray.api.banditproblemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanditproblemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
