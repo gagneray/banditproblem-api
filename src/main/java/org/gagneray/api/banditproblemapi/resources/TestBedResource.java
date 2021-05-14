5827444d2f2ba10bf2345376552f218e59f9b37d
gagneray <50414560+gagneray@users.noreply.github.com>
2021-05-14 22:48:32 +0200

    get only test bed results

--- src/main/java/org/gagneray/api/banditproblemapi/resources/TestBedResource.java
+++ src/main/java/org/gagneray/api/banditproblemapi/resources/TestBedResource.java
@@ -3,7 +3,8 @@ package org.gagneray.api.banditproblemapi.resources;
 
 import org.gagneray.api.banditproblemapi.services.TestBedService;
 import org.gagneray.rl.banditproblem.dto.TestBedConfigurationDTO;
-import org.gagneray.rl.banditproblem.services.BanditProblemTestBed;
+import org.gagneray.rl.banditproblem.services.BanditProblemTestBed.TestBedResultDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +20,8 @@ public class TestBedResource {
     }
 
     @GetMapping("/testbed")
-    public BanditProblemTestBed processTestBed(@RequestBody @NonNull TestBedConfigurationDTO testBedConfigurationDTO) {
-        return testBedService.processTestBed(testBedConfigurationDTO);
+    public ResponseEntity<TestBedResultDTO> processTestBed(@RequestBody @NonNull TestBedConfigurationDTO testBedConfigurationDTO) {
+        return ResponseEntity.ok(testBedService.processTestBed(testBedConfigurationDTO));
     }
 
 }
--- src/main/java/org/gagneray/api/banditproblemapi/services/TestBedService.java
+++ src/main/java/org/gagneray/api/banditproblemapi/services/TestBedService.java
@@ -3,16 +3,16 @@ package org.gagneray.api.banditproblemapi.services;
 import org.gagneray.rl.banditproblem.configurations.TestBedConfiguration;
 import org.gagneray.rl.banditproblem.dto.TestBedConfigurationDTO;
 import org.gagneray.rl.banditproblem.services.BanditProblemTestBed;
+import org.gagneray.rl.banditproblem.services.BanditProblemTestBed.TestBedResultDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestBedService {
 
-
-    public BanditProblemTestBed processTestBed(TestBedConfigurationDTO testBedConfigurationDTO) {
+    public TestBedResultDTO processTestBed(TestBedConfigurationDTO testBedConfigurationDTO) {
         TestBedConfiguration testBedConfiguration = new TestBedConfiguration(testBedConfigurationDTO);
         BanditProblemTestBed testBed = new BanditProblemTestBed(testBedConfiguration);
         testBed.process();
-        return testBed;
+        return testBed.getResults();
     }
 }
