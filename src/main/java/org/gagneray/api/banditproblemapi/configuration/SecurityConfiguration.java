9fff623abeb5895ceb21ba3630d0511d0d1dfe5f
Gaetan Agneray <agneray.g@gmail.com>
2021-05-31 13:36:40 +0200

    allow anonymous call to testbed endpoint

---
+++ src/main/java/org/gagneray/api/banditproblemapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package org.gagneray.api.banditproblemapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/testbed")
+                .permitAll()
+
+                //default spring security config
+                .and()
+                .authorizeRequests((requests) -> {
+                    ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated();
+                });
+        http.formLogin();
+        http.httpBasic();
+    }
+}
