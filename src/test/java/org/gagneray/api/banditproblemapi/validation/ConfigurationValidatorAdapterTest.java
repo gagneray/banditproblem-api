4273aec3b38b80463d2061d089acc58fa41f85aa
gagneray <agneray.g@gmail.com>
2022-05-30 14:26:37 +0200

    validation test againts properties

--- src/test/java/org/gagneray/api/banditproblemapi/validation/ConfigurationValidatorAdapterTest.java
+++ src/test/java/org/gagneray/api/banditproblemapi/validation/ConfigurationValidatorAdapterTest.java
@@ -1,6 +1,7 @@
 package org.gagneray.api.banditproblemapi.validation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.gagneray.api.banditproblemapi.configuration.TestBedProperties;
 import org.gagneray.rl.banditproblem.dto.TestBedConfigurationDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,10 @@ class ConfigurationValidatorAdapterTest {
     @Autowired
     private ConfigurationValidatorAdapter configurationValidatorAdapter;
 
-    private static final String JSON_CONFIG_PATH = "src/test/java/org/gagneray/api/banditproblemapi/resources/testBedConfigurations/testbed_config_1.json";
+    @Autowired
+    private TestBedProperties testBedProperties;
+
+    private static final String JSON_CONFIG_PATH = "src/test/java/resources/testbed_config_1.json";
 
 
     @Test
@@ -33,6 +37,28 @@ class ConfigurationValidatorAdapterTest {
         TestBedConfigurationDTO testBedConfigurationDTO = createTestBedConfigurationDTO(JSON_CONFIG_PATH);
         MapBindingResult result = configurationValidatorAdapter.validate(testBedConfigurationDTO, false);
         assertFalse(result.hasErrors());
+    }
+
+    @Test
+    void ValidationTestAgaintsProperties() throws IOException {
+        TestBedConfigurationDTO testBedConfigurationDTO = createTestBedConfigurationDTO(JSON_CONFIG_PATH);
+
+        testBedConfigurationDTO.setBanditProblemCount(testBedProperties.getMaxBanditProblemCount() + 1);
+        testBedConfigurationDTO.setTotalSteps(testBedProperties.getMaxTotalSteps() + 1);
+        testBedConfigurationDTO.setK(testBedProperties.getMaxBanditPerBanditProblem() + 1);
+
+        for (int i = 0; i < testBedProperties.getMaxPoliciesCount() + 1; i++) {
+            testBedConfigurationDTO.getPolicies().add(testBedConfigurationDTO.getPolicies().get(0));
+        }
+
+        // Standard validation
+        MapBindingResult result = configurationValidatorAdapter.validate(testBedConfigurationDTO, false);
+        assertFalse(result.hasErrors());
+
+        // Restricted Validation
+        result = configurationValidatorAdapter.validate(testBedConfigurationDTO, true);
+        assertTrue(result.hasErrors());
+        assertEquals(4, result.getErrorCount());
     }
 
     private TestBedConfigurationDTO createTestBedConfigurationDTO(String jsonConfigPath) throws IOException {
